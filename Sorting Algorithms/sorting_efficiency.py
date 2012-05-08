7c365d78f73449e5a3cf57339d08dc752af87789
Patrick Murphy <murph.pm@gmail.com>
2012-05-08 12:59:47 -0500

    Added merge sort algorithm

--- Sorting Algorithms/sorting_efficiency.py
+++ Sorting Algorithms/sorting_efficiency.py
@@ -60,6 +60,43 @@ def quick_sort_rec(seq, max_v, min_v):
 		quick_sort_rec(seq, pivot - 1, small)
 		quick_sort_rec(seq, max_v, big)
 
+def merge_sort(seq):
+	merge_sort_rec(seq, 0, len(seq), [])
+	
+def merge_sort_rec(seq, min_v, max_v, tmp):
+	if max_v > min_v + 1:
+		mid_v = (max_v - min_v) / 2 + min_v
+		merge_sort_rec(seq, min_v, mid_v, tmp)
+		merge_sort_rec(seq, mid_v, max_v, tmp)
+		merge(seq, min_v, mid_v, max_v, tmp)
+		
+def merge(seq, min_v, mid_v, max_v, tmp):
+	curr_l = min_v
+	curr_r = mid_v
+	curr_t = min_v
+	tmp = [ i for i in seq ]
+	while curr_l < mid_v or curr_r < max_v:
+		if seq[curr_l] <= seq[curr_r]:
+			tmp[curr_t] = seq[curr_l]
+			curr_l += 1
+		else:
+			tmp[curr_t] = seq[curr_r]
+			curr_r += 1
+		curr_t += 1
+	if curr_l < mid_v:
+		while curr_l < mid_v:
+			tmp[curr_t] = seq[curr_l]
+			curr_t += 1
+			curr_l += 1
+	elif curr_r < max_v:
+		while curr_r < max_v:
+			tmp[curr_t] = seq[curr_r]
+			curr_t += 1
+			curr_r += 1
+	for i in range(max_v, len(seq)):
+		tmp[i] = seq[i]
+	seq = [ i for i in tmp ]
+	
 
 def generate_seq(max_v, min_v, length):
 	to_return = []
