e5f06571870ae86916dc460981c4340303122a29
Patrick Murphy <murph.pm@gmail.com>
2012-06-06 18:03:02 -0500

    started working on a queue that is implemented with a circular linked list

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class CircularLinkedList
+    {
+        Node dummyNode;
+        int size;
+
+        public CircularLinkedList()
+        {
+            dummyNode = new Node("dUmMy");
+            dummyNode.setNext(dummyNode);
+            size = 0;
+        }
+
+
+
+    }
+
+    class Node
+    {
+        private string data;
+        private Node next;
+
+        public Node(string newData)
+        {
+            data = newData;
+            next = null;
+        }
+
+        public void setNext(Node newNext)
+        {
+            next = newNext;
+        }
+
+        public string getData()
+        {
+            return data;
+        }
+
+        public Node getNext()
+        {
+            return next;
+        }
+
+        public string toString()
+        {
+            return data;
+        }
+
+        public bool Equals(string toTest)
+        {
+            return String.Equals(data, toTest);
+        }
+    }
+}
